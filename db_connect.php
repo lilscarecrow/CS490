e1ab76fb424f3a631711fe3cd0ca0cb86d1b162e
Ron Pastori <RPastori@users.noreply.github.com>
2016-09-28 21:49:16 -0400

    back-end files
    Establishes connection to my MySQL database on NJIT's sql2 server.

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * db_connect.php
+ * Created by PhpStorm.
+ * User: ron
+ * Date: 9/27/16
+ */
 
 define('DB_USER', 'rp387');
 define('DB_PASSWORD', 'zptBa5ve');
-define('DB_HOST', 'sql2.njit.edu'); //128.235.211.21 10.80.0.11 sql2.njit.edu
+define('DB_HOST', 'sql2.njit.edu');
 define('DB_NAME', 'rp387');
 
 $db_connect = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)

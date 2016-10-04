bfb77d45822561f7b9933bad1e6a0ca46da59398
Ron Pastori <RPastori@users.noreply.github.com>
2016-10-03 20:42:16 -0400

    Switched from procedural to object-oriented

--- db_connect.php
+++ db_connect.php
@@ -11,9 +11,7 @@ define('DB_PASSWORD', 'zptBa5ve');
 define('DB_HOST', 'sql2.njit.edu');
 define('DB_NAME', 'rp387');
 
-$db_connect = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-            or die('Could not connect. ' . mysqli_connect_error() . "\n");
-
-//$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); //I'm going to switch to the mysqli object soon.
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+      or die('Could not connect. ' . $db->connect_error . "\n");
 
 ?>
